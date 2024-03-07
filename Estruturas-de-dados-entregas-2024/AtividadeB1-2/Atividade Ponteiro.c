f6db8558d878b218df4f5405e60548926f1864ca
Bruno Martinho <128179417+Bruno-Martinho@users.noreply.github.com>
2024-03-07 17:10:25 -0300

    Add files via upload

--- Estruturas-de-dados-entregas-2024/AtividadeB1-2/Atividade Ponteiro.c
+++ Estruturas-de-dados-entregas-2024/AtividadeB1-2/Atividade Ponteiro.c
@@ -1,58 +1,31 @@
 #include <stdio.h>
 
-void applyDiscount(int* price, double* discount) {
-  
-    if (*price <= 30 ){
-        *discount = 0.2;
+void applyDiscount(int *price, float discount) {
 
-      *price = *price - (*price * *discount);
+      *price -= *price * (discount / 100);
 
-      *discount = *discount * 100;
-    }
-
-    else if (*price >= 31 && *price <= 50 ){
-          *discount = 0.4;
-
-        *price = *price - (*price * *discount);
-
-        *discount = *discount * 100;
-      }
-    else if (*price >= 51 ){
-        *discount = 0.5;
-
-      *price = *price - (*price * *discount);
-
-      *discount = *discount * 100;
-    }
-      
-    else{
-      *discount = 0.0;
-    }
 }
 
 int main(void) {
 
   int precocomDesconto;
   int precosemDesconto;
-  double desconto;
+  float desconto;
 
   printf("Coloque o preço do produto: \n");
   scanf("%d", &precocomDesconto);
+  
+  printf("Coloque o desconto: \n");
+  scanf("%f", &desconto);
 
   //salva o preço sem desconto
   precosemDesconto = precocomDesconto;
   
-  applyDiscount(&precocomDesconto, &desconto);
-  
-  if ( desconto == 0.0 ){
-    printf("O preço do produto: %d\n", precosemDesconto);
-    printf("O produto não possui desconto\n");
-  }
-  else {
-    printf("O preço do produto sem desconto: %d\n", precosemDesconto);
-    printf("O preço do produto com desconto: %d\n", precocomDesconto);
-    printf("Valor do desconto aplicado: %.f%%\n", desconto);
-  }
+  applyDiscount(&precocomDesconto, desconto);
+
+  printf("O preço do produto sem desconto: %d\n", precosemDesconto);
+  printf("O preço do produto com desconto: %d\n", precocomDesconto);
+  printf("Valor do desconto aplicado: %.2f%%\n", desconto);
   
   return 0;
 }
